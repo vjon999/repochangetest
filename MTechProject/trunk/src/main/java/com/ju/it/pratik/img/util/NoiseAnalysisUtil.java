3cd048da69cb877580ffdc47e0f3aa7780634039
pratik006@gmail.com <pratik006@gmail.com@ec55034b-2391-1314-85a8-7e5c9041a640>
2014-05-03 18:07:16 +0000

    added Bit Error Rate measure

--- MTechProject/trunk/src/main/java/com/ju/it/pratik/img/util/NoiseAnalysisUtil.java
+++ MTechProject/trunk/src/main/java/com/ju/it/pratik/img/util/NoiseAnalysisUtil.java
@@ -1,49 +1,30 @@
 package com.ju.it.pratik.img.util;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
+import com.ju.it.pratik.img.Image;
 
 public class NoiseAnalysisUtil {
 	
 	public NoiseAnalysisResult calculatePSNR(String originalImageLoc, String watermarkedImageLoc) throws IOException {		
-		BufferedImage bufferedImage = ImageIO.read(new File(originalImageLoc));
-		int imageWidth = bufferedImage.getWidth();
-		int imageHeight = bufferedImage.getHeight();
-		int[] originalImage = new int[imageWidth*imageHeight];
-		bufferedImage.getRGB(0, 0, imageWidth, imageHeight, originalImage, 0, imageWidth);
-		int[] rgb = new int[imageWidth * imageHeight];
-		bufferedImage.getRGB(0, 0, imageWidth, imageHeight, rgb, 0, imageWidth);
-		int y[] = new int[rgb.length];
-		int u[] = new int[rgb.length];
-		int v[] = new int[rgb.length];
-		TransformUtils.rgb2yuv(rgb, y, u, v);
-		
-		bufferedImage = ImageIO.read(new File(watermarkedImageLoc));
-		imageWidth = bufferedImage.getWidth();
-		imageHeight = bufferedImage.getHeight();
-		int[] resultImage = new int[imageWidth*imageHeight];
-		bufferedImage.getRGB(0, 0, imageWidth, imageHeight, resultImage, 0, imageWidth);
-		int[] rgb1 = new int[imageWidth * imageHeight];
-		bufferedImage.getRGB(0, 0, imageWidth, imageHeight, rgb1, 0, imageWidth);
-		int y1[] = new int[rgb.length];
-		int u1[] = new int[rgb.length];
-		int v1[] = new int[rgb.length];
-		TransformUtils.rgb2yuv(rgb1, y1, u1, v1);
-		return calculatePSNR(ImageUtils.to2D(y, imageHeight, imageWidth), ImageUtils.to2D(y1, imageHeight, imageWidth));
+		Image origLogo = new Image(originalImageLoc);
+		Image extractedLogo = new Image(watermarkedImageLoc);
+		return calculatePSNR(origLogo.getY(), extractedLogo.getY());
 	}
 
 	public NoiseAnalysisResult calculatePSNR(int[][] originalImage, int[][] watermarkedImage) {
 		int nrows = originalImage[0].length, ncols = originalImage.length;
 		double signal = 0, noise = 0, peak = 0;
+		int error = 0;
 		NoiseAnalysisResult result = new NoiseAnalysisResult();
 
 		for (int i = 0; i < nrows; i++) {
 			for (int j = 0; j < ncols; j++) {
 				//System.out.println(originalImage[i][j]+"\t"+watermarkedImage[i][j]);
 				signal += originalImage[i][j] * originalImage[i][j];
+				if(originalImage[i][j] != watermarkedImage[i][j]) {
+					error++;
+				}
 				noise += Math.pow((originalImage[i][j] - watermarkedImage[i][j]), 2);
 				if (peak < watermarkedImage[i][j])
 					peak = watermarkedImage[i][j];
@@ -57,6 +38,8 @@ public class NoiseAnalysisUtil {
 		result.setSnr(10*Math.log10(signal/noise));
 		double ncc = new CorrelationCalculator().calcNormalizedCrossCorrelation(originalImage, watermarkedImage);
 		result.setNcc(ncc);
+		double ber = (double)error/(nrows*ncols);
+		result.setBer(ber);
 		return result;
 	}
 	
