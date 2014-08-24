29a2b95cfa542b4dc3a247d96d999e8e7a284aff
pratik006 <pratik006@ec55034b-2391-1314-85a8-7e5c9041a640>
2014-08-24 19:40:26 +0000

    Working copy of UCI Server after converting it to xml based configuration

---
+++ tags/UCIHost20131020/UCIHost/src/com/chess/uci/share/AsyncReader.java
@@ -0,0 +1,52 @@
+package com.chess.uci.share;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Logger;
+
+
+public class AsyncReader implements Runnable {
+
+	private static Logger LOG = Logger.getLogger(AsyncReader.class.getName());
+	
+	private NetworkRW networkRW;
+	private volatile boolean stop = false;
+	private OutputStream os;
+	
+	public AsyncReader(OutputStream os, NetworkRW networkRW, Boolean exit) {
+		this.os = os;
+		this.networkRW = networkRW;
+	}
+	
+	@Override
+	public void run() {
+		StringBuffer message = new StringBuffer();
+		String msg = null;
+		try {
+			while (!stop && (msg = networkRW.readFromNetwork()) != null) {
+				message.append(msg);
+				if(message.lastIndexOf("\n") != message.length()-1) {
+					message.append("\n");
+				}
+				LOG.finer("client: " + message+"\tLength: "+message);
+				os.write(message.toString().getBytes());
+				os.flush();
+				message = new StringBuffer();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			stop = true;
+			try {
+				os.write("quit\n".getBytes());
+				os.flush();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}	
+		}
+		LOG.info("Closing "+getClass().getName());
+	}
+	
+	public void stop() {
+		stop = true;
+	}
+}
---
+++ tags/UCIHost20131020/UCIHost/src/mail/MailHandler.java
@@ -0,0 +1,98 @@
+package mail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailHandler implements Runnable {
+
+	private boolean sent = false;
+	
+	public boolean isSent() {
+		return sent;
+	}
+
+	@Override
+	public void run() {
+		try {
+			send("pratik006@gmail.com", "vickysengupta006@gmail.com", "Server_IP="+getPublicIP(), "");
+			sent = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			sent = false;
+		}
+	}
+	
+	private static String getPublicIP() throws IOException {
+		URL whatismyip = new URL("http://automation.whatismyip.com/n09230945.asp");
+	    URLConnection connection = whatismyip.openConnection();
+	    connection.addRequestProperty("Protocol", "Http/1.1");
+	    connection.addRequestProperty("Connection", "keep-alive");
+	    connection.addRequestProperty("Keep-Alive", "1000");
+	    connection.addRequestProperty("User-Agent", "Web-Agent");
+
+	    BufferedReader in = 
+	        new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+	    String ip = in.readLine(); //you get the IP as a String
+	    return ip;
+	}
+
+	private static void send(String from, String to, String subject, String content){
+   	 
+		final String username = "vickysengupta006@gmail.com";
+		final String password = "chinat0wn";
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.timeout","30000");
+		
+		Session mailSession = Session.getInstance(props,
+				  new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				  });
+ 
+		Message simpleMessage = new MimeMessage(mailSession);
+ 
+		InternetAddress fromAddress = null;
+		InternetAddress toAddress = null;
+		try {
+			fromAddress = new InternetAddress(from);
+			toAddress = new InternetAddress(to);
+		} catch (AddressException e) {
+			e.printStackTrace();
+		}
+ 
+		try {
+			simpleMessage.setFrom(fromAddress);
+			simpleMessage.setRecipient(RecipientType.TO, toAddress);
+			simpleMessage.setSubject(subject);
+			simpleMessage.setText(content);
+ 
+			Transport.send(simpleMessage);
+			System.out.println("sent");
+			
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
