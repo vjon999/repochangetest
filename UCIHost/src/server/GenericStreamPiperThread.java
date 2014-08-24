13b45fe9b293084879d54df784d0781146314f7c
pratik006 <pratik006@ec55034b-2391-1314-85a8-7e5c9041a640>
2014-08-24 19:40:26 +0000

    Working copy of UCI Server after converting it to xml based configuration

---
+++ UCIHost/src/server/GenericStreamPiperThread.java
@@ -0,0 +1,37 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class GenericStreamPiperThread implements Runnable {
+
+	public static final int DEFAULT_BUFFER_SIZE = 1024*1000;
+	
+	private InputStream is;
+	private OutputStream os;
+	
+	public GenericStreamPiperThread(InputStream is, OutputStream os) {
+		this.is = is;
+		this.os = os;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			streamPiper(is, os);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void streamPiper(InputStream is, OutputStream os) throws IOException {
+		byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+		int readLen = 0;
+		while((readLen = is.read(buffer, 0, buffer.length)) != -1) {
+			os.write(buffer, 0, readLen);
+			os.flush();
+		}
+	}
+
+}
---
+++ UCIHost/util/com/util/ProtocolConstants.java
@@ -0,0 +1,44 @@
+package com.util;
+
+public interface ProtocolConstants {
+	
+	public static final int DEFAULT_ADMIN_PORT = 11000;
+	public static final int BUFFER_SIZE = 1024*5;
+	
+	public static final String AUTHENTICATE = "authenticate";
+	public static final String AUTHENTICATION_SUCCESSFUL = "authentication_success";
+	public static final String AUTHENTICATION_FAIL = "authentication_failed";
+	
+	public static final String CLOSE_CONNECTION = "close_connection";
+	public static final String GET_PORT = "get_port";
+	public static final String CHANGE_PORT = "change_port";
+	public static final String PORT = "port";
+	
+	public static final String ENGINE_NAME = "engine_name";
+	public static final String AVAILABLE_ENGINES = "available_engines";
+	public static final String GET_AVAILABLE_ENGINES = "get_available_engines";
+	public static final String TEST_CONNECTION = "test_connection";
+	public static final String SUCCESSFUL_CONNECTION_MESSAGE = "success";
+	public static final String SUCCESSFUL_ENGINE_SELECTION_MESSAGE = "successfully_engine_selected";
+	public static final String HELLO_PACKET = "helloPacket";
+	
+	public static final String DELIMITER = ",";
+	
+	public static final String SAVE_CONFIGURATION = "save_config";
+	public static final String SAVE_SUCCESSFUL = "save_successful";
+    public static final String SAVE_FAILED = "save_failed";
+	public static final String SELECTED_ENGINE = "selected_engine";
+	
+	public static final String UNKNOWN_COMMAND = "unknown_command";
+	
+	
+	
+	public static final String START_MSG = "helloserver";
+	public static final String CONN_SUCCESS_MSG = "connected";
+	public static final String SET_PROTOCOL_TCP = "protocol=tcp";
+	public static final String SET_PROTOCOL_UDP = "protocol=udp";
+	public static final String SUCCESS_SET_PROTOCOL_TCP = "tcp protocol set";
+	public static final String SUCCESS_SET_PROTOCOL_UDP = "udp protocol set";
+	public static final String QUIT_MSG = "quit";
+	public static final String CLOSE_MSG = "close";
+}
